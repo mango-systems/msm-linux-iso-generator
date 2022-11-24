feaba3d043bb67911927cacf60dd0601cf1d3dfd
MSM <msm74588@gmail.com>
2022-11-25 02:30:30 +0530

    disabled "old kernel removal section"

--- create-iso-from-scratch.sh
+++ create-iso-from-scratch.sh
@@ -225,18 +225,18 @@ sudo apt-get purge thunderbird*
 rm /var/lib/dbus/machine-id
 
 # remove diversion
-rm /sbin/initctl
+# rm /sbin/initctl
 dpkg-divert --rename --remove /sbin/initctl
 
 # Remove upgraded, old linux-kernels if more than one: (FOR FAILSAFE, may throw error)
-ls /boot/vmlinuz-2.6.**-**-generic > list.txt
-sum=$(cat list.txt | grep '[^ ]' | wc -l)
+# ls /boot/vmlinuz-2.6.**-**-generic > list.txt
+# sum=$(cat list.txt | grep '[^ ]' | wc -l)
 
-if [ $sum -gt 1 ]; then
-dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
-fi
+# if [ $sum -gt 1 ]; then
+# dpkg -l 'linux-*' | sed '/^ii/!d;/'"$(uname -r | sed "s/\(.*\)-\([^0-9]\+\)/\1/")"'/d;s/^[^ ]* [^ ]* \([^ ]*\).*/\1/;/[0-9]/!d' | xargs sudo apt-get -y purge
+# fi
 
-rm list.txt
+# rm list.txt
 
 # clean up
 apt-get clean
